ff5d7f02103e1aeca5e69cd880eea08d4e3de0c1
曉風 <97892311+Akatsuki030@users.noreply.github.com>
2023-09-17 09:57:03 +0800

    Update train.py

--- train.py
+++ train.py
@@ -29,6 +29,7 @@ from losses import generator_loss, discriminator_loss, feature_loss, kl_loss
 from mel_processing import mel_spectrogram_torch, spec_to_mel_torch
 from text.symbols import symbols
 
+hps.model_dir = "/content/drive/MyDrive"
 
 torch.backends.cudnn.benchmark = True
 global_step = 0
